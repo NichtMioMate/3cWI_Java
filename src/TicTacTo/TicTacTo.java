6a198c6efde36b306031ab4a615a335bcfc56e70
NichtMioMate <ichigonyt2113@gmail.com>
2022-05-18 08:49:02 +0200

    TicTacTo 04 v0.1

---
+++ src/TicTacTo/TicTacTo.java
@@ -0,0 +1,14 @@
+package TicTacTo;
+
+import java.util.Scanner;
+
+public class TicTacTo {
+    public static void main(String[] args) {
+
+        Scanner tictac = new Scanner(System.in);
+
+
+        String input = tictac.next();
+
+    }
+}
